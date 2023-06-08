092923051bee3ac762240bc12f1549e11bf8e1ee
Michal Převrátil <michprev@gmail.com>
2023-06-08 14:34:50 +0200

    ✨ Add networks config

---
+++ scripts/config.py
@@ -0,0 +1,11 @@
+
+networks = {
+    "Goerli": "https://ethereum-goerli.publicnode.com",
+    "BSC Testnet": "https://bsc-testnet.publicnode.com",
+    "Polygon Mumbai": "https://endpoints.omniatech.io/v1/matic/mumbai/public",
+    "Avalanche Fuji": "https://avalanche-fuji-c-chain.publicnode.com",
+    "Arbitrum Goerli": "https://arbitrum-goerli.publicnode.com",
+    "Polygon zkEVM Testnet": "https://rpc.public.zkevm-test.net",
+    "Optimism Goerli": "https://goerli.optimism.io",
+    "Base Goerli": "https://goerli.base.org",
+}

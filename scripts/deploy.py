9217e9be2f9842e2493f016e1521437a2bcb0057
Michal Převrátil <michprev@gmail.com>
2023-06-08 14:44:49 +0200

    💡 Add comments to `deploy.py`

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -2,6 +2,14 @@ from woke.deployment import *
 
 NODE_URL = "ENTER_NODE_URL_HERE"
 
+# [ ] Install node dependencies
+# [ ] `woke init pytypes`
+# [ ] Run `check_balances` script
+# [ ] Change EVM version to Paris
+# [ ] Create user interface using `pick`
+# [ ] Deploy dummy with ERC1967Proxy
+# [ ] Create upgrade script
+
 
 @default_chain.connect(NODE_URL)
 def main():
